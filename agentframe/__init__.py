84fe2a2bf0c757bee4d433c0a5f8d2516d55eb8e
otix <otix@yoga14s>
2026-07-17 23:33:07 +0800

    chore: disable litellm remote model cost map fetch on import

--- agentframe/__init__.py
+++ agentframe/__init__.py
@@ -1,3 +1,8 @@
+import os as _os
+
+# litellm: 禁用远程模型价格表拉取，用本地缓存即可
+_os.environ.setdefault("LITELLM_LOCAL_MODEL_COST_MAP", "True")
+
 from .core.agent import Agent
 from .tools.function_tool import function_tool
 
